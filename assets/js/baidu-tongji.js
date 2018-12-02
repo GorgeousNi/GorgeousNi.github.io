6a88aef9a6c6774ae113fa3e8d25bf6583ecca89
GorgeousNi <gorgeousni@qq.com>
2018-12-02 16:26:34 +0800

    增加百度统计

---
+++ assets/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5c6697f44a73d09b4a6e8f5d2c659537";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
